e56ac23e154d74700fb2d449d2ea8ad82fbf9ac4
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2025-03-05 11:02:14 +0530

    Completed customer money-transfer page - route only

---
+++ src/app/features/customer/money-transfer/money-transfer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-money-transfer',
+  templateUrl: './money-transfer.component.html',
+})
+export class MoneyTransferComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
